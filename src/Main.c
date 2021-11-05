243a755366029b56ec0e01680f2c6acaadaa85e7
Thanh Duy <55396370+silverbossTD@users.noreply.github.com>
2021-11-05 21:19:42 +0700

    Add lexer / token.

--- src/Main.c
+++ src/Main.c
@@ -1,6 +1,17 @@
+#include "../include/Lexer.h"
+
 #include <stdio.h>
 
 int main(int argc, char const *argv[]) {
-        printf("Hello World!\n");
+        lexer_T *lexer = init_lexer(
+                "var hello =\"Hello World\";\n"
+                "print(hello);\n"
+        );
+
+        token_T *token = (void*)0;
+
+        while ((token = lexer_get_next_token(lexer)) != (void*)0) {
+                printf("TOKEN(%d, %s)\n", token->type, token->value);
+        }
         return 0;
 }
